fbed535378ec94fcc752c36bf13a2d3ffb30dd29
Nicky Johnstone <nicky@qcode.co.uk>
2022-12-08 10:06:37 +0000

    Rename line length check workflow

--- scripts/line-length-check.tcl
+++ scripts/line-length-check.tcl
@@ -1,27 +1,22 @@
-try {
-    set long_lines false
+set long_lines false
 
-    foreach filename [glob [lindex $argv 0]] {
-        set handle [open $filename r]
-        set contents [read $handle]
-        set line_no 1
+foreach filename [glob [lindex $argv 0]] {
+    set handle [open $filename r]
+    set contents [read $handle]
+    set line_no 1
 
-        foreach line [split $contents "\n"] {
-            if { [string length $line] > 90 } {
-                puts "[file tail $filename] :: line $line_no"
-                set long_lines true
-            }
-
-            incr line_no
+    foreach line [split $contents "\n"] {
+        if { [string length $line] > 90 } {
+            puts "[file tail $filename] :: line $line_no"
+            set long_lines true
         }
 
-        close $handle
+        incr line_no
     }
 
-    if { $long_lines } {
-        exit 1
-    }
-} on error [list message options] {
-    puts "Error: $message"
+    close $handle
+}
+
+if { $long_lines } {
     exit 1
 }
