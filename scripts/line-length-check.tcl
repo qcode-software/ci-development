514d8e53044d2305c49e453237f8f47659fe2e83
Nicky Johnstone <nicky@qcode.co.uk>
2022-12-12 17:56:56 +0000

    Added a test for linter_report_lines_over_length

--- scripts/line-length-check.tcl
+++ scripts/line-length-check.tcl
@@ -1,11 +1,14 @@
-set repository [lindex $argv 0]
-set files [lrange $argv 1 end]
+set max_line_length [lindex $argv 0]
+set repository [lindex $argv 1]
+set files [lrange $argv 2 end]
 set long_lines [list]
 
 source "${repository}/tcl/linter.tcl"
 
 foreach file $files {
-    set lines [linter_report_lines_over_length "${repository}/${file}" 90]
+    set lines [linter_report_lines_over_length \
+                   "${repository}/${file}" \
+                   $max_line_length]
     lappend long_lines {*}$lines
 
     foreach line $lines {
