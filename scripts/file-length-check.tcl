9c0aafc443b4bd70366e340b347f90a20687904d
Nicky Johnstone <nicky@qcode.co.uk>
2023-05-05 17:26:58 +0100

    EOd

--- scripts/file-length-check.tcl
+++ scripts/file-length-check.tcl
@@ -3,7 +3,9 @@ set max_lines [lindex $argv 1]
 set repository [lindex $argv 2]
 set files [lrange $argv 3 end]
 
-source $linter
+global auto_path
+lappend auto_path [file join $repository packages]
+package require qcode-ci
 
 puts "Checking for files that are more than $max_lines lines long."
 
